666a4eeafc8e4a45de7a73e90fc537233f63c3c8
Kavyapal Singh <kavyapalsingh44@gmail.com>
2026-02-03 12:55:35 +0530

    Adding background image and writing text on image and align the text so it show nice

--- main.py
+++ main.py
@@ -1,15 +1,32 @@
 from PIL import Image ,ImageDraw,ImageFont,ImageFilter
 
-img = Image.new("RGB", (3000, 500), color="white")
+background = Image.open("background/background.jpeg").convert("RGB")
 
-draw = ImageDraw.Draw(img)
+background = background.resize((1200, 1600))
+
+draw = ImageDraw.Draw(background)
+
 
 font = ImageFont.truetype("fonts/QEDavidReidCAP.ttf", 40)
-draw.text((50, 50), "Hello World", fill="black", font=font)
 
-font = ImageFont.truetype("fonts/QEHerbertCooper.ttf", 40)
-draw.text((2000, 50), "Hello World", fill="black", font=font)
+# Text input
+text = """Hello World
+This text is aligned
+with notebook lines
+and looks natural"""
 
-img.save("output.png")
+lines = text.split("\n")
 
-img.show()
+# Starting position (margin)
+
+x, y = 10, 150 # the x and y where written line is start
+line_gap = 95  # the margin between lines
+
+
+for line in lines:    # how the text input is printing 
+    draw.text((x, y), line, fill=(0, 0, 0), font=font)
+    y += line_gap
+
+background.save("output.png")  # the output img is saved 
+
+background.show()
